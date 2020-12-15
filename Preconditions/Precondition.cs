d50636692608616fc24ffe5baf8b04f69aae4dc5
René Wiersma <rene.wiersma@noordhoff.nl>
2020-12-15 13:29:25 +0100

    Initial commit

---
+++ Preconditions/Precondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReneWiersma.Preconditions
+{
+    public static class Precondition
+    {
+        public static void Requires(bool precondition, string message = "Precondition failed") => 
+            Requires(precondition, () => new ArgumentException(message));
+
+        public static void Requires<TException>(bool precondition, Func<TException> func) where TException : Exception
+        {
+            if (!precondition)
+                throw func.Invoke();
+        }
+    }
+}
---
+++ PreconditionsTests/PreconditionTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using ReneWiersma.Preconditions;
+using System;
+
+namespace PreconditionsTests
+{
+    public class PreconditionTests
+    {
+        [Test]
+        public void PreconditionTrue()
+        { 
+            Assert.That(() => Precondition.Requires(true), Throws.Nothing);
+        }
+
+        [Test]
+        public void PreconditionFalse()
+        {
+            Assert.That(() => Precondition.Requires(false), Throws.ArgumentException.With.Message.EqualTo("Precondition failed"));
+        }
+
+        [Test]
+        public void PreconditionFalseWithCustomExceptionMessage()
+        {
+            Assert.That(() => Precondition.Requires(false, "A custom message"), Throws.ArgumentException.With.Message.EqualTo("A custom message"));
+        }
+
+        [Test]
+        public void PreconditionFalseWithAnotherExceptionType()
+        {
+            Assert.That
+            (
+                () => Precondition.Requires(false, () => new InvalidOperationException("A custom message")), 
+                Throws.InvalidOperationException.With.Message.EqualTo("A custom message")
+            );
+        }
+    }
+}
